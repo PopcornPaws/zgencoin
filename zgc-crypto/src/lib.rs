f9613c19225b1707ac430ada2295a0c49ee479fb
Mark Melczer <melczer7@gmail.com>
2021-07-15 10:15:31 +0200

    Hello bello.

--- zgc-crypto/src/lib.rs
+++ zgc-crypto/src/lib.rs
@@ -1,3 +1,5 @@
+// TODO #![feature(array_chunks)]
+//
 //fn sha256(input: String) -> [u8; 32] {
 //    // STEP 1 >> Preprocessing
 //    // 1) convert to binary
@@ -16,8 +18,30 @@
 //    // STEP 4 >> concatenate final hash
 //}
 
-fn sha256(input: String) -> [u8; 32] {
+mod consts;
+use consts::{HASH_VALUES, ROUND_CONSTANTS};
+
+pub fn sha256(input: String) -> [u8; 32] {
     let processed_input_vec = preprocess(input);
+    //let message_schedule = processed_input_vec
+    //    .array_chunks::<4>()
+    //    .map(|chunk| u32::from_be_bytes(*chunk))
+    //    .collect::<Vec<u32>>();
+    let mut message_schedule = Vec::<u32>::new();
+    for i in 0..processed_input_vec.len() / 4 - 1 {
+        let mut u32_bytes = [0_u8; 4];
+        u32_bytes.copy_from_slice(&processed_input_vec[4 * i..4 * i + 4]);
+        message_schedule.push(u32::from_be_bytes(u32_bytes));
+    }
+
+    //let message_schedule = processed_input_vec
+    //    .chunks(4)
+    //    .map(|chunk| {
+    //        let mut u32_bytes = [0_u8; 4];
+    //        u32_bytes.copy_from_slice(chunk);
+    //        u32::from_be_bytes(u32_bytes)
+    //    })
+    //    .collect::<Vec<u32>>();
     todo!();
 }
 
@@ -31,10 +55,11 @@ fn preprocess(input: String) -> Vec<u8> {
         input_bytes.push(0_u8)
     }
 
+    // original length in bits
     let original_length_in_bits = (original_length * 8).to_be_bytes();
-
+    // last 8 bytes
     let copy_range = input_bytes.len() - 8..input_bytes.len();
-
+    // copy the original length (in bits) to the last 64 bits of the padded data
     input_bytes[copy_range].copy_from_slice(&original_length_in_bits); // panics if lengths are not equal
 
     input_bytes
@@ -60,5 +85,9 @@ mod test {
         assert_eq!(preprocessed_data.len(), 128);
         assert_eq!(preprocessed_data[100], 0b1000_0000);
         assert_eq!(preprocessed_data[120..], [0_u8, 0, 0, 0, 0, 0, 3, 32]); // length is 800 in bits
+
+        for element in &preprocessed_data[101..120] {
+            assert_eq!(*element, 0_u8);
+        }
     }
 }
