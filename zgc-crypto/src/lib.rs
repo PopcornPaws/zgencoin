41ed04245315aca46397d23aa040e5998d48a2c7
Mark Melczer <melczer7@gmail.com>
2021-07-21 18:20:22 +0200

    Small changes.

--- zgc-crypto/src/lib.rs
+++ zgc-crypto/src/lib.rs
@@ -1,14 +1,14 @@
 #![feature(array_chunks)]
-// STEP 1 >> Preprocessing
-// 1) convert to binary
-// 2) append a single 1 bit
-// 3) pad with 0 until length is a multiple of 512
-// 4) replace last 64 bytes with the input data length
-// STEP 2 >> Initialization of auxiliary hash variables
+// STEP 1 >> Initialization of auxiliary hash variables
 // 1) first 32 bits of the fractional part of the
 //    square root of the first 8 primes (2, 3, 5, 7, 11, 13, 17, 19)
 // 2) first 32 bits of the fractional part of the
 //    cubic root of the first 64 primes (2, 3, 5, 7, 11, ..., 311)
+// STEP 2 >> Preprocessing
+// 1) convert to binary
+// 2) append a single 1 bit
+// 3) pad with 0 until length is a multiple of 512
+// 4) replace last 64 bytes with the input data length
 // STEP 3 >> for every 512 bit chunk:
 // 1) create a message schedule
 // 2) compression
@@ -54,7 +54,7 @@ fn right_rotate(num: u32, by: usize) -> u32 {
 fn preprocess(input: String) -> Vec<u8> {
     // as_bytes vs into_bytes
     let mut input_bytes = input.into_bytes(); // Vec<u8>
-    let original_length = input_bytes.len(); // in bytes!
+    let input_bytes_len = input_bytes.len(); // in bytes!
 
     input_bytes.push(0b1000_0000);
     while input_bytes.len() % 64 != 0 {
@@ -62,7 +62,7 @@ fn preprocess(input: String) -> Vec<u8> {
     }
 
     // original length in bits
-    let original_length_in_bits = (original_length * 8).to_be_bytes();
+    let original_length_in_bits = (input_bytes_len * 8).to_be_bytes();
     // last 8 bytes
     let copy_range = input_bytes.len() - 8..input_bytes.len();
     // copy the original length (in bits) to the last 64 bits of the padded data
