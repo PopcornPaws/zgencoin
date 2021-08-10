f571360bcecbeca3c9ac7f2c2d6caa3c722b2118
Mark Melczer <melczer7@gmail.com>
2021-08-10 16:53:09 +0200

    Fixed example

--- zgc-common/examples/hash_serde.rs
+++ zgc-common/examples/hash_serde.rs
@@ -5,8 +5,12 @@ use std::fs::File;
 fn main() {
     let path_to_json = "examples/hash_test.json";
     let reader = File::open(path_to_json).expect("Failed to open file");
-    let address = Address::new([2; 20]);
-    println!("serialized = {:?}", serde_json::to_vec(&address).unwrap());
+    let address = Address::try_from_str("abcdef01234567899876543210abcdef01234567")
+        .expect("invalid address string provided");
+    println!(
+        "serialized = {:?}",
+        serde_json::to_string(&address).unwrap()
+    );
     let address: Address = serde_json::from_reader(reader).expect("failed to deserialize address");
 
     println!("parsed = {:?}", address);
